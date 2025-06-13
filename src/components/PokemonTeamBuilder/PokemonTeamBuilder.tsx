d8c713aaabfddafbff4a865c96f4605e2e1bff7e
Renan Alonso <renan.alonso91@gmail.com>
2025-06-13 01:39:23 -0300

    refactor: reusing component for type damage relations

--- src/components/PokemonTeamBuilder/PokemonTeamBuilder.tsx
+++ src/components/PokemonTeamBuilder/PokemonTeamBuilder.tsx
@@ -179,7 +179,11 @@ export default function PokemonTeamBuilder() {
     <div className='min-h-screen bg-gray-900 text-white flex flex-col items-center p-8'>
       <h1 className='text-3xl font-bold mb-8'>Pokémon Team Builder</h1>
 
-      <TypeInfo data={calculateTeamWeaknesses(team)} />
+      <div className='mb-12 w-full max-w-4xl'>
+        <h2 className='text-2xl font-bold mb-4'>Team Overall Weaknesses:</h2>
+
+        <TypeInfo data={calculateTeamWeaknesses(team)} />
+      </div>
 
       <div className='grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-4xl'>
         {team.map((pokemon, index) => (
@@ -252,6 +256,16 @@ export default function PokemonTeamBuilder() {
                     Weaknesses / Resistances / Immunities:
                   </h3>
 
+                  <TypeInfo
+                    data={calculateDamageMultipliers(pokemon.data.types)}
+                  />
+                </div>
+
+                {/* <div className='mt-6 w-full max-w-sm'>
+                  <h3 className='font-semibold mb-2'>
+                    Weaknesses / Resistances / Immunities:
+                  </h3>
+
                   <div className='grid grid-cols-6 gap-4'>
                     {Object.entries(
                       calculateDamageMultipliers(pokemon.data.types),
@@ -273,7 +287,7 @@ export default function PokemonTeamBuilder() {
                       </div>
                     ))}
                   </div>
-                </div>
+                </div> */}
 
                 <div className='mt-4 text-sm text-gray-300'>
                   <h3 className='font-semibold mb-1'>
