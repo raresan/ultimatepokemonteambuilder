70ad1ef159bf1270f1d0dbcb58b83e8baa3a937c
Renan Alonso <renan.alonso91@gmail.com>
2025-08-03 23:39:56 -0300

    feat: updating language selector, keeping url parameters

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -1,15 +1,26 @@
+'use client'
+
 import Link from 'next/link'
 import { dictionaries } from '@/i18n/dictionaries'
 import { Fragment } from 'react'
+import { useSearchParams } from 'next/navigation'
 
 export default function LanguageSelector() {
+  const searchParams = useSearchParams()
+
   return (
-    <ul className='flex gap-1 p-2 justify-end'>
+    <ul className='flex gap-1 p-2 fixed top-0 right-0'>
       {Object.keys(dictionaries)?.map((lang, index) => (
         <Fragment key={index}>
           {index > 0 && <li>|</li>}
           <li className='uppercase'>
-            <Link href={`/${lang}`}>{lang}</Link>
+            <Link
+              href={`/${lang}${
+                searchParams.toString() ? `?${searchParams.toString()}` : ''
+              }`}
+            >
+              {lang}
+            </Link>
           </li>
         </Fragment>
       ))}
